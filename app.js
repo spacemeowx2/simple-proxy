7c458c352d50149d025e1cedf00b8dd730893226
spacemeowx2 <spacemeowx2@gmail.com>
2018-04-24 00:39:58 +0800

    change port to 22083

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const SelectorHandler = require('./handler/selector').SelectorHandler
 
 async function main () {
   const wss = new WebSocket.Server({
-    port: 8080
+    port: 22083
   })
   wss.on('connection', function connection(ws) {
     const handler = new SelectorHandler(ws)
@@ -11,6 +11,6 @@ async function main () {
       .then(() => ws.close())
       .catch(e => console.error('error', e.message))
   })
-  console.log('Listening to port 8080')
+  console.log('Listening to port 22083')
 }
 main().catch(e => console.error(e))
